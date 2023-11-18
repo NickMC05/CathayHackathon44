d98d19329ae37db8c1062aff74d3312b9ab3e2bf
jam-khan <jamkhan@connect.hku.hk>
2023-11-18 19:55:02 +0800

    Chat UI finished, with different colors and box for AI and User response

--- lib/pages/chat_ai.dart
+++ lib/pages/chat_ai.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
+enum MessageType { user, ai }
+
+class ChatMessage {
+  final String text;
+  final MessageType type;
+
+  ChatMessage({required this.text, required this.type});
+}
+
+
 void main() => runApp(ChatApp());
 
 class ChatApp extends StatelessWidget {
@@ -12,46 +22,66 @@ class ChatApp extends StatelessWidget {
   }
 }
 
+
 class ChatScreen extends StatefulWidget {
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  final List<String> _messages = [];
+  final List<ChatMessage> _messages = [];
   final TextEditingController _textController = TextEditingController();
 
   void _handleSubmitted(String text) {
     _textController.clear();
     setState(() {
-      _messages.insert(0, text);
+      _messages.insert(0, ChatMessage(text: text, type: MessageType.user));
+      // Hardcoded AI response
+      _messages.insert(0, ChatMessage(text: "AI Response to '$text'", type: MessageType.ai));
     });
-    // Send text to the backend AI model
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('ChatBot')),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              reverse: true,
-              itemCount: _messages.length,
-              itemBuilder: (context, index) => ListTile(
-                title: Text(_messages[index]),
-              ),
-            ),
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: Text('ChatBot')),
+    body: Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            reverse: true,
+            itemCount: _messages.length,
+            itemBuilder: (context, index) {
+              final message = _messages[index];
+              return ListTile(
+                title: Align(
+                  alignment: message.type == MessageType.user
+                      ? Alignment.centerRight
+                      : Alignment.centerLeft,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: message.type == MessageType.user
+                          ? Colors.blue[100]
+                          : Colors.grey[300],
+                    ),
+                    child: Text(message.text),
+                  ),
+                ),
+              );
+            },
           ),
-          Divider(height: 1.0),
-          Container(
-            child: _buildTextComposer(),
-          ),
-        ],
-      ),
-    );
-  }
+        ),
+        Divider(height: 1.0),
+        Container(
+          child: _buildTextComposer(),
+        ),
+      ],
+    ),
+  );
+}
+
 
   Widget _buildTextComposer() {
     return IconTheme(
