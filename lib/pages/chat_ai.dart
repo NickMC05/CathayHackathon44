e6135dcda141435dc6aa88c20358cee483620747
Nick MC <n1ch0l45wk@gmail.com>
2023-11-19 01:21:21 +0800

    ai

--- lib/pages/chat_ai.dart
+++ lib/pages/chat_ai.dart
@@ -1,7 +1,5 @@
 import 'package:cathay/services/gpt3_service.dart';
 import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-
 
 enum MessageType { user, ai }
 
@@ -12,7 +10,7 @@ class ChatMessage {
   ChatMessage({required this.text, required this.type});
 }
 
-// void main() => runApp(ChatApp());
+void main() => runApp(ChatApp());
 
 class ChatApp extends StatelessWidget {
   @override
@@ -61,7 +59,7 @@ class _ChatScreenState extends State<ChatScreen> {
       '''},
       {"role": "user", "content": text}
     ]).then(
-      (value) {
+          (value) {
         print(value);
         setState(() {
           _messages.add(ChatMessage(text: value, type: MessageType.ai));
@@ -69,57 +67,60 @@ class _ChatScreenState extends State<ChatScreen> {
       },
     );
   }
- @override
+
+  @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       child: SafeArea(
         child: Column(
           children: [
             Expanded(
-              child: ListView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: _messages.length,
-                itemBuilder: (context, index) {
-                  final message = _messages[index];
-                  return Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
-                    child: Align(
-                      alignment: message.type == MessageType.user
-                          ? Alignment.centerRight
-                          : Alignment.centerLeft,
-                      child: Container(
-                        padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: message.type == MessageType.user
-                              ? Color(0xFF00645A)
-                              : CupertinoColors.systemGrey5,
-                        ),
-                        child: Text(
-                          message.text,
-                          style: TextStyle(
+              child: SingleChildScrollView(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: _messages.length,
+                  itemBuilder: (context, index) {
+                    final message = _messages[index];
+                    return Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                      child: Align(
+                        alignment: message.type == MessageType.user
+                            ? Alignment.centerRight
+                            : Alignment.centerLeft,
+                        child: Container(
+                          padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
                             color: message.type == MessageType.user
-                                ? CupertinoColors.white
-                                : CupertinoColors.black,
+                                ? Color(0xFF00645A)
+                                : CupertinoColors.systemGrey5,
+                          ),
+                          child: Text(
+                            message.text,
+                            style: TextStyle(
+                              color: message.type == MessageType.user
+                                  ? CupertinoColors.white
+                                  : CupertinoColors.black,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             ),
-            _buildCupertinoTextComposer(),
+            _buildInputRow(),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildCupertinoTextComposer() {
+  Widget _buildInputRow() {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
       child: Row(
         children: [
           SizedBox(width: 16.0),
@@ -133,7 +134,7 @@ class _ChatScreenState extends State<ChatScreen> {
           CupertinoButton(
             child: Icon(
               CupertinoIcons.paperplane,
-              color: Color(0xFF00645A)
+              color: Color(0xFF00645A),
             ),
             onPressed: () => _handleSubmitted(_textController.text),
           ),
