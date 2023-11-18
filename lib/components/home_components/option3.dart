934e0cecda83acc79224e6ec880733e8b71598e6
RichieWilynnton <richiewillynton@gmail.com>
2023-11-18 17:55:45 +0800

    changed to cupertino

--- lib/components/home_components/option3.dart
+++ lib/components/home_components/option3.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class Option3 extends StatelessWidget {
   const Option3({ super.key });
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: const Color(0xFF2DBD3A));
+    return Container(color: CupertinoColors.black);
   }
 }
